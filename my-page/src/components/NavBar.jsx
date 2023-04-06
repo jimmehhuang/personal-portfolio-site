1a1b57a1e8ca8f4fcd04775c65c7f4bf473c119f
Jimmy Huang <jimmymhuang@gmail.com>
2023-04-06 17:27:48 -0500

    fixed popout functionality

--- my-page/src/components/NavBar.jsx
+++ my-page/src/components/NavBar.jsx
@@ -10,19 +10,19 @@ const NavBar = ({updatePage}) => {
 
     const navLinks = [
         {
-            id: 1,
+            id: 0,
             link: 'About'
         },
         {   
-            id: 2,
+            id: 1,
             link: 'Projects'
         },
         {
-            id: 3,
+            id: 2,
             link: 'Experience'
         },
         {
-            id: 4,
+            id: 3,
             link: 'Contact'
         }
     ]
@@ -34,18 +34,16 @@ const NavBar = ({updatePage}) => {
                     <li key={id} className="p-4" onClick={() => updatePage(id)}>{link}</li>
                     ))}
             </ul>
-            <div onClick={changeNav} className="block md:hidden">
+            <div onClick={changeNav} className="p-2 block md:hidden">
                 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}                
             </div>
-            <div className={!nav ? "fixed left-0 top-0 w-{60%} bg-gray-800 ease-in-out duration-500" : "fixed left-[-100%] ease-in-out duration-500"}>
-                <a className="p-4" onClick={() => updatePage(0)}>JH</a>
-               <ul className="pt-16">
+            <div className={!nav ? "fixed right-0 top-12 w-{60%} bg-gray-800 ease-in-out duration-500" : "fixed right-[-100%] top-12 ease-in-out duration-500"}>
+               <ul className="p-2">
                 {navLinks.map(({id, link}) => (            
                     <li key={id} className="p-4 border-b border-gray-500" onClick={() => updatePage(id)}>{link}</li>
                     ))}
             </ul> 
-            </div>
-            
+            </div>            
         </div>
     )
 }
