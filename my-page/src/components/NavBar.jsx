e7df343f8ea0d0a8b9812a065c97d49026647b1a
Jimmy Huang <jimmymhuang@gmail.com>
2023-04-06 17:21:55 -0500

    fixing navbar functionality

--- my-page/src/components/NavBar.jsx
+++ my-page/src/components/NavBar.jsx
@@ -1,7 +1,12 @@
 import React, {useState} from "react"
-import {RxHamburgerMenu} from 'react-icons/rx'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const NavBar = ({updatePage}) => {
+    const [nav, setNav] = useState(false);
+
+    const changeNav = () => {
+        setNav(!nav);
+    }
 
     const navLinks = [
         {
@@ -23,13 +28,24 @@ const NavBar = ({updatePage}) => {
     ]
     return (
         <div className='flex justify-between items-center'>
-            <a className="" onClick={() => updatePage(0)}>JH</a>
-                <RxHamburgerMenu className=""/>
-            <ul className="flex">
+            <a className="p-4" onClick={() => updatePage(0)}>JH</a>
+            <ul className="hidden md:flex">
                 {navLinks.map(({id, link}) => (            
                     <li key={id} className="p-4" onClick={() => updatePage(id)}>{link}</li>
                     ))}
-                </ul>
+            </ul>
+            <div onClick={changeNav} className="block md:hidden">
+                {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}                
+            </div>
+            <div className={!nav ? "fixed left-0 top-0 w-{60%} bg-gray-800 ease-in-out duration-500" : "fixed left-[-100%] ease-in-out duration-500"}>
+                <a className="p-4" onClick={() => updatePage(0)}>JH</a>
+               <ul className="pt-16">
+                {navLinks.map(({id, link}) => (            
+                    <li key={id} className="p-4 border-b border-gray-500" onClick={() => updatePage(id)}>{link}</li>
+                    ))}
+            </ul> 
+            </div>
+            
         </div>
     )
 }
